f5fcfb196b3905eae513f67985d34d90c97a7a6d
taesongkim <taesongkim@gmail.com>
2015-03-20 15:29:33 -0400

    initial commit

---
+++ Thread/Thread/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Thread
+//
+//  Created by Justin on 3/20/15.
+//  Copyright (c) 2015 Justin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Thread/Thread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thread
+//
+//  Created by Justin on 3/20/15.
+//  Copyright (c) 2015 Justin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
